3eefb01f03473021b0eb4b2a10929ed2606d2d2d
GalaxyJun <52619309+GalaxyJun@users.noreply.github.com>
2023-12-12 15:25:40 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '阿骏说家电', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '高端家电评测+技术分享', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://vivazz7.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '索尼电视, 奈飞', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '索尼,电视,奈飞,apple TV,安卓', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'activince7@gmail.com', // 邮箱地址 例如mail@tangly1024.com
